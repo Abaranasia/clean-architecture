748de88fb53ab50443fc78eea1384cb8f02a273b
Abaranasia <rankirlian@gmail.com>
2024-04-23 19:45:44 +0200

    Feat: add Entity base class

---
+++ practice/practice01/domain/entities/entity.ts
@@ -0,0 +1,26 @@
+import { Id } from "../value-objects/Id";
+
+export interface EntityData { id: Id };
+
+const isEntity = (v: any): v is Entity => v instanceof Entity;
+
+export abstract class Entity implements EntityData {
+  protected constructor(public id: Id) { }
+
+  public equals(object?: Entity): boolean {
+    if (object === null || object == undefined) {
+      return false;
+    }
+
+    if (this === object) {
+      return true;
+    }
+
+    if (!isEntity(object)) {
+      return false;
+    }
+
+    return this.id.equals(object.id);
+  }
+
+}
