e77f7b6f2e2966787fd8680205c6c46add8e3417
Abaranasia <rankirlian@gmail.com>
2024-04-23 20:36:00 +0200

    Refactor: improve id value-object get method and update tests

--- practice/practice01/__tests__/domain/value-objects/Id.test.ts
+++ practice/practice01/__tests__/domain/value-objects/Id.test.ts
@@ -4,15 +4,15 @@ describe('Tests of Name class methods', () => {
 
   test('should create a valid id', () => {
     const user = Id.create()
-    expect(user.getId).not.toBe('')
-    expect(user.getId).not.toBeUndefined()
+    expect(user.id).not.toBe('')
+    expect(user.id).not.toBeUndefined()
   })
 
   test('should create different ids each time', () => {
     const user1 = Id.create()
     const user2 = Id.create()
-    const userId1 = user1.getId();
-    const userId2 = user2.getId();
+    const userId1 = user1.id;
+    const userId2 = user2.id;
 
     expect(userId1 !== userId2)
     expect(user1.equals(user2)).toBe(false)
--- practice/practice01/domain/value-objects/Id.ts
+++ practice/practice01/domain/value-objects/Id.ts
@@ -1,22 +1,17 @@
 import { ValueObject } from "./ValueObject";
-import { uidGenerator } from "./shared/helpers";
+import { uidGenerator } from "./shared/utils";
 
 export interface IdProps {
   id: string
 }
 
-
 export class Id extends ValueObject<IdProps> {
-  // id: string;
 
   private constructor(props: IdProps) {
     super(props)
-    // this.id = props.id;
-    // console.log(`The id ${this.id} has been succesfully created`)
   }
 
-  public getId(): string {
-    console.log(`Your id is ${this.props.id}`);
+  public get id(): string {
     return this.props.id;
   }
 
