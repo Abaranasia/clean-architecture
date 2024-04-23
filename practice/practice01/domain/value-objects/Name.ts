9c743a70a5acebc2354716108bcf58323757ccbf
Abaranasia <rankirlian@gmail.com>
2024-04-23 23:50:31 +0200

    Chore: add comment in Name class

--- practice/practice01/domain/value-objects/Name.ts
+++ practice/practice01/domain/value-objects/Name.ts
@@ -15,6 +15,7 @@ export class Name extends ValueObject<NameProps> {
     return this.props.name;
   }
 
+  // Not usable since value-objects should be inmutable
   public set name(name: string) {
     console.log(`Your name is ${this.props.name}`);
     if (typeof (name) != "string" || !name || name.trim().length === 0) {
