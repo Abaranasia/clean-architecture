a2be83d1c8741715c53d5dc7e144f4a9bf751180
Abaranasia <rankirlian@gmail.com>
2024-05-19 19:43:00 +0200

    Feat: add get and remove methods to Users class

--- practice/practice01/app/repositories/users.ts
+++ practice/practice01/app/repositories/users.ts
@@ -1,9 +1,9 @@
 import { 
-    UserData, 
     UserObjectData, 
     UsersListData, 
 } from "../../domain/entities/shared/users.interface";
 import { User } from "../../domain/entities/user";
+import { Id } from "../../domain/value-objects/Id";
 
 export class Users {
     public users: UsersListData;
@@ -12,15 +12,11 @@ export class Users {
         this.users = [user];
     }
 
-    public static create(props: UserData): Users {
-        const user = User.create(props);
-        
+    public static create(user: UserObjectData): Users {
         return new Users(user)
     } 
 
-    public addUser(props: UserData): void {
-        const user = User.create(props);
-        
+    public addUser(user: UserObjectData): void {      
         this.users = [
             ...this.users,
             user,
@@ -30,4 +26,18 @@ export class Users {
     public getUsers(): UsersListData {
         return this.users
       }
+
+    public getById(id: string): User | unknown {
+        const user =  this.users.find((user) => user.id.id=== id)
+        return user 
+    }
+
+    public getByEmail(email: string): User | unknown {
+        const user =  this.users.find((user) => user.email.email=== email)
+        return user 
+    }
+
+    public removeUser(userId: Id): void {
+        this.users.filter(user => user.id != userId); 
+    }
 }
