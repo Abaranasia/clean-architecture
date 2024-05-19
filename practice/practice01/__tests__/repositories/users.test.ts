f4b628bab2a0533ca39b21f1e2cfd64c7b688bc1
Abaranasia <rankirlian@gmail.com>
2024-05-19 19:43:19 +0200

    Chore: add and fix Users tests

--- practice/practice01/__tests__/repositories/users.test.ts
+++ practice/practice01/__tests__/repositories/users.test.ts
@@ -1,4 +1,5 @@
 import { Users } from "../../app/repositories/users";
+import { User } from "../../domain/entities/user";
 
 describe('Tests of Users entity methods', () => {
     const userMock1 = {
@@ -14,21 +15,40 @@ describe('Tests of Users entity methods', () => {
     };  
 
     test('should create a valid list of users', () => {
-      const userList=Users.create(userMock1)
-      const result = userList.users.filter((user) => user.email.email === userMock1.email)
+      const newUser = User.create(userMock1)
+
+      const userList=Users.create(newUser)
+      const result = userList.users.filter((user) => user.id === newUser.id)
       
       expect(userList.users.length).not.toBe(0);
       expect(result).not.toEqual({});
-      });
+    });
 
     test('should be able to add new users to a list of users', () => {
-      const userList=Users.create(userMock1)
+      const newUser1 = User.create(userMock1)
+      const newUser2 = User.create(userMock2)
+
+      const userList=Users.create(newUser1)
       const userList1 =  userList.getUsers();
 
-      userList.addUser(userMock2)
+      userList.addUser(newUser2)
       const userList2 =  userList.getUsers();
       
       expect(userList1).not.toEqual(userList2);
       expect(userList.users.length).toBe(2)
-      });
-    })
+    }); 
+
+    test('should return a user from a list of users by its id', () => {
+      const newUser = User.create(userMock1)
+      const userList=Users.create(newUser)
+
+      expect (userList.getById(newUser.id.id)).toEqual(newUser)
+    });
+  
+    test('should return a user from a list of users by its email', () => {
+      const newUser = User.create(userMock1)
+      const userList=Users.create(newUser)
+
+      expect (userList.getByEmail(newUser.email.email)).toEqual(newUser)
+    });
+  })
