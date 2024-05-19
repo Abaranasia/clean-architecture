463e9228b9ce5968811251551278cde0134d2a33
Abaranasia <rankirlian@gmail.com>
2024-05-19 19:44:40 +0200

    Style:getAll output improvement

--- practice/practice01/app/adapters/iUserRepository.ts
+++ practice/practice01/app/adapters/iUserRepository.ts
@@ -1,10 +1,10 @@
-import { UserData } from "../../domain/entities/shared/users.interface";
+import { UserData, UsersListData } from "../../domain/entities/shared/users.interface";
 import { User } from "../../domain/entities/user";
 
 export interface IUserRepository {
     getByEmail(email: string): Promise<User | unknown>;     
     getById(id: string): Promise<User | unknown>;
-    getAll(): Promise<User[]>;
+    getAll(): Promise<UsersListData | unknown>;
     createUser(newUser: UserData): Promise<User>;
     updateUser(newUser: UserData): Promise<User>; 
 }
