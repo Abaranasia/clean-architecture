069a1328b8b6634b55a4dc032a2d24208a0275cc
Abaranasia <rankirlian@gmail.com>
2024-05-21 20:34:45 +0200

    Chore: add UserStorage class replacing Users

---
+++ app/data/userStorage.ts
@@ -0,0 +1,41 @@
+import { User } from "../domain/entities/user";
+import { Id } from "../domain/value-objects/Id";
+
+export class UserStorage {
+    public users: User[];
+
+    private constructor (user: User) {
+        this.users = [user];
+    }
+
+    public static create(user: User): User[] {
+        return new UserStorage(user)
+    }  
+
+    public addUser(user: User): void {      
+        this.users = [
+            ...this.users,
+            user,
+            ];
+    }
+
+    public getUsers(): User[] {
+        return this.users
+      }
+
+    public getById(id: string): User | null {
+        const user =  this.users.find((user) => user.id.id=== id)
+        if (!user) return null
+        return user 
+    }
+
+    public getByEmail(email: string): User | null {
+        const user =  this.users.find((user) => user.email.email=== email)
+        if (!user) return null
+        return user 
+    }
+
+    public removeUser(userId: Id): void {
+        this.users= this.users.filter(user => user.id !== userId); 
+    }
+}
