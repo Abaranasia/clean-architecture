68fe1e2b7a38de0174f9a49f2ba1e8e8002f4312
Abaranasia <rankirlian@gmail.com>
2024-05-21 20:36:39 +0200

    Refactor. userRepository rework and cleanup

--- app/domain/repositories/userRepository.ts
+++ app/domain/repositories/userRepository.ts
@@ -1,39 +1,24 @@
 
 import { IUserRepository } from "../../adapters/iUserRepository";
-import { UserData, UsersListData } from "../../domain/entities/shared/users.interface";
+import { UserData } from "../../domain/entities/shared/users.interface";
 import { User } from "../../domain/entities/user";
-import { Users } from "./users";
+import { UserStorage } from "../../data/userStorage";
 
 export class UserRepository implements IUserRepository {
 
-    constructor (private users: Users) {}
+    constructor (private userStorage: UserStorage) {}
 
-    async getByEmail(email: string): Promise<User | unknown> {
-        return this.users.getByEmail(email)
+    async getByEmail(email: string): Promise<User | null> {
+        return this.userStorage.getByEmail(email)
       }
 
-      async getById(id: string): Promise<User | unknown> {
-        return this.users.getById(id);
-      }
-
-      async getAll(): Promise<UsersListData | unknown> {
-        return this.users.getUsers()
+      async getAll(): Promise<User[] | null> {
+        return this.userStorage.getUsers()
       }
 
       async createUser(newUser: UserData): Promise<User> {
         const user =  User.create(newUser);
-        this.users.addUser(user)
+        this.userStorage.addUser(user)
         return user
       }
-
-      async updateUser(newUser: UserData): Promise<User> {
-        const user = User.update(newUser);
-
-        // First we remove the user from users
-        this.users.removeUser(user.id)
-        // Then we add the update user to users
-        this.users.addUser(user)
-
-        return user
-      } 
 }
