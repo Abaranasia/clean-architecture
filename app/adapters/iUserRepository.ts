d061f76f54190cfde2f67caf331b668c9d8b8cff
Abaranasia <rankirlian@gmail.com>
2024-05-21 23:59:28 +0200

    Fix: remove value return on createUser interface

--- app/adapters/iUserRepository.ts
+++ app/adapters/iUserRepository.ts
@@ -4,7 +4,7 @@ import { User } from "../domain/entities/user";
 export interface IUserRepository {
     getByEmail(email: string): Promise<User | null>;     
     getAll(): Promise<User[] | null>;
-    createUser(newUser: UserData): Promise<User>;
+    createUser(newUser: UserData): void;
     // getById(id: string): Promise<User | null>;
     // updateUser(newUser: UserData): Promise<User>; 
 }
