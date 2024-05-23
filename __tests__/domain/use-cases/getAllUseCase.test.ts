0db83a5d504f9942513e394a1b6fbd239fedb06b
Abaranasia <rankirlian@gmail.com>
2024-05-23 20:31:19 +0200

    Chore: add getAllUseCase tests

---
+++ __tests__/domain/use-cases/getAllUseCase.test.ts
@@ -0,0 +1,14 @@
+import { GetAllUseCase } from "../../../app/useCases/getAllUseCase";
+import { mockUserRepository } from "../../__mocks__/mockUserRepository";
+import { mockUser1, mockUser2 } from "../../__mocks__/userEntity";
+
+describe('Tests of getAll Use Case', () => {
+
+    test('should return a list of all users', async () => {
+        const userCase = new GetAllUseCase(mockUserRepository);
+        const result = await userCase.execute()
+
+        expect(mockUserRepository.getAll).toHaveBeenCalled();
+        expect(result).toEqual([mockUser1, mockUser2]);
+    });
+});
