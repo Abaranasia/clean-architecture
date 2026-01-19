1e2a4c05e9e23e8da2eb08cf1c36dcf7c22d2f27
Abaranasia <rankirlian@gmail.com>
2026-01-19 20:39:49 +0100

    style: format document

--- app/data/api/userStorage.ts
+++ app/data/api/userStorage.ts
@@ -1,39 +1,34 @@
-
 import { User } from "../../domain/entities/users/user";
 import { Id } from "../../domain/value-objects/Id";
 
 export class UserStorage {
-    public users: User[];
+  public users: User[];
 
-    public constructor () {
-        this.users = []
-    }
+  public constructor() {
+    this.users = [];
+  }
 
+  public addUser(user: User): void {
+    this.users = [...this.users, user];
+  }
 
-    public addUser(user: User): void {      
-        this.users = [
-            ...this.users,
-            user,
-            ];
-    }
+  public getUsers(): User[] {
+    return this.users;
+  }
 
-    public getUsers(): User[] {
-        return this.users
-      }
+  public getById(id: string): User | null {
+    const user = this.users.find((user) => user.id.id === id);
+    if (!user) return null;
+    return user;
+  }
 
-    public getById(id: string): User | null {
-        const user =  this.users.find((user) => user.id.id=== id)
-        if (!user) return null
-        return user 
-    }
+  public getByEmail(email: string): User | null {
+    const user = this.users.find((user) => user.email.email === email);
+    if (!user) return null;
+    return user;
+  }
 
-    public getByEmail(email: string): User | null {
-        const user =  this.users.find((user) => user.email.email=== email)
-        if (!user) return null
-        return user 
-    }
-
-    public removeUser(userId: Id): void {
-        this.users= this.users.filter(user => user.id !== userId); 
-    }
+  public removeUser(userId: Id): void {
+    this.users = this.users.filter((user) => user.id !== userId);
+  }
 }
