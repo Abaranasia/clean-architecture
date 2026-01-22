70fe0e95797a622a414fd1b6061642bd6308e7ee
Abaranasia <rankirlian@gmail.com>
2026-01-22 19:59:35 +0100

    style: indentation

--- src/domain/useCases/getAllUseCase.ts
+++ src/domain/useCases/getAllUseCase.ts
@@ -2,17 +2,14 @@ import { User } from "../entities/users/user";
 import { UserRepository } from "../repositories/UserRepository";
 
 export class GetAllUseCase {
-    constructor (
-        private userRepository: UserRepository
-    )
-    {}
-    async execute (): Promise<User[] | null> {
-        try {
-            const userList = await this.userRepository.getAll()
-            
-            return userList;
-        } catch (error) {
-            throw new Error ('Something happened when requesting the users')
-        }
+  constructor(private userRepository: UserRepository) {}
+  async execute(): Promise<User[] | null> {
+    try {
+      const userList = await this.userRepository.getAll();
+
+      return userList;
+    } catch (error) {
+      throw new Error("Something happened when requesting the users");
     }
+  }
 }
