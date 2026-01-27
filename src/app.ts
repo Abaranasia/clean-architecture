70c6885adb53d44d4a38b7dd74a6d44b0f1b09ab
Abaranasia <rankirlian@gmail.com>
2026-01-27 20:38:11 +0100

    ref: extract dependencies to compositionRoot and refactor main function

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,5 @@
-import { UserController } from "./presentation/controllers/UserController";
-import { CreateUserUseCase } from "./domain/useCases/createUserUseCase";
-import { GetAllUseCase } from "./domain/useCases/getAllUseCase";
-import { UserApiRepository } from "./data/users/userRepository";
-import { UserStorage } from "./data/api/userStorage";
+import { userController } from "./compositionRoot";
 import { User, UserData } from "./domain/entities/users/user";
-
-// Set up dependencies
-const userStorage = new UserStorage();
-const userRepository = new UserApiRepository(userStorage);
-const createUserUseCase = new CreateUserUseCase(userRepository);
-const getAllUseCase = new GetAllUseCase(userRepository);
-const userController = new UserController(createUserUseCase, getAllUseCase);
 
 const listUsers = (userList: User[]) => {
   userList.forEach((user, index) => {
@@ -20,25 +9,32 @@ const listUsers = (userList: User[]) => {
   });
 };
 
-// Example usage
-async function main(newUsers: UserData[]) {
+const createInitialUsers = async (newUsers: UserData[]) => {
   try {
-    // Create all users from the newUsers array
     for (const userData of newUsers) {
       await userController.createUser(userData);
-    }
-
-    // Get and display all users
-    const allUsers = await userController.getAllUsers();
-    if (allUsers) {
-      console.log("\nAll Users:");
-      listUsers(allUsers);
-    } else {
-      console.log("No users found");
-    }
+    };
+    listAllUsers()
   } catch (error) {
     console.error("An error occurred:", error);
   }
-}
+};
 
-export { userController, main };
+const listAllUsers = async () => {
+  const allUsers = await userController.getAllUsers();
+  if (allUsers) {
+    console.log("\nExisting Users:");
+    listUsers(allUsers);
+  } else {
+    console.log("No users found");
+  }
+};
+
+const addNewUser = async (userData: UserData) => {
+  try {
+    await userController.createUser(userData);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+export { createInitialUsers, listAllUsers, addNewUser };
