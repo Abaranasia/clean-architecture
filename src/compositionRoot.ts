dccb5df8fef556e88b4e999f04ed9ec9b8d51554
Abaranasia <rankirlian@gmail.com>
2026-01-27 22:44:23 +0100

    chore: add getUserByEmail to CompositionRoot

--- src/compositionRoot.ts
+++ src/compositionRoot.ts
@@ -1,6 +1,7 @@
 // import { UserController } from "./presentation/controllers/UserController";
 import { CreateUserUseCase } from "./domain/useCases/createUserUseCase";
 import { GetAllUseCase } from "./domain/useCases/getAllUseCase";
+import { GetByEmailUseCase } from "./domain/useCases/getByEmailUseCase";
 import { UserApiRepository } from "./data/users/userRepository";
 import { UserStorage } from "./data/api/userStorage";
 import { User, UserData } from "./domain/entities/users/user";
@@ -9,11 +10,13 @@ export const userStorage = new UserStorage();
 export const userRepository = new UserApiRepository(userStorage);
 export const createUserUseCase = new CreateUserUseCase(userRepository);
 export const getAllUseCase = new GetAllUseCase(userRepository);
+export const getByEmailUseCase = new GetByEmailUseCase(userRepository);
 
 class UserController {
   constructor(
     private createUserUseCase: CreateUserUseCase,
     private getAllUseCase: GetAllUseCase,
+    private getByEmailUseCase: GetByEmailUseCase,
   ) {}
 
   async createUser(userData: UserData): Promise<void> {
@@ -24,6 +27,11 @@ class UserController {
       const users = await this.getAllUseCase.execute();
       return users;
   }
+
+  async getUserByEmail(email: string): Promise<User | null> {
+    const user = await this.getByEmailUseCase.execute(email);
+    return user;
+  }
 }
 
-export const userController = new UserController(createUserUseCase, getAllUseCase);
+export const userController = new UserController(createUserUseCase, getAllUseCase, getByEmailUseCase);
